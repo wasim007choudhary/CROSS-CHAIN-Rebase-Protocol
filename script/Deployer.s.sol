a73cdc299245cb6badf558df9c55956bec68a4cb
Wasim Choudhary <wasimchoudhary@Mac.lan>
2025-11-03 00:11:42 +0530

    Fixed import paths

--- script/Deployer.s.sol
+++ script/Deployer.s.sol
@@ -9,8 +9,9 @@ import {CCRToken} from "../src/CCRebaseToken.sol";
 import {CCRebaseTokenPool} from "../src/CCRebaseTokenPool.sol";
 import {CCIPLocalSimulatorFork, Register} from "lib/chainlink-local/src/ccip/CCIPLocalSimulatorFork.sol";
 import {IERC20} from "lib/ccip/contracts/src/v0.8/vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/IERC20.sol";
+
 import {RegistryModuleOwnerCustom} from
-    "lib/ccip/contracts/src/v0.8/ccip/tokenAdminRegistry/RegistryModuleOwnerCustom.sol";
+    "lib/chainlink-local/lib/ccip/contracts/src/v0.8/ccip/tokenAdminRegistry/RegistryModuleOwnerCustom.sol";
 import {TokenAdminRegistry} from "lib/ccip/contracts/src/v0.8/ccip/tokenAdminRegistry/TokenAdminRegistry.sol";
 
 contract DeployTokenAndPool is Script {
